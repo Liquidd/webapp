24a639bb088ccc6b5f6e5f5728d5b21021274826
Luis Manuel Rojo de Santiago <luis_rojo240@hotmail.com>
2019-11-14 12:06:13 -0700

    nombre de archivos corregidos en mayusculas

---
+++ application/controllers/Controlador_general.php
@@ -0,0 +1,59 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class Controlador_general extends CI_Controller {
+
+    protected $name_user;
+    protected $permisos;
+    protected $estado_sesion;
+    public function __construct(){
+        parent::__construct();
+        $this->load->library('session');
+        $this->load->helper('url');
+
+        if ($this->session->userdata('login_state') !== FALSE){
+            $this->id_usuario = $this->session->userdata("datos_usuario")['id_usuario'];
+            $this->name_user = $this->session->userdata("datos_usuario")['username'];
+            $this->permisos = $this->session->userdata("datos_usuario")['permisos'];
+            $this->estado_sesion = $this->session->userdata("login_state");
+
+		}else{
+            $this->load->view("login");
+        }
+    }
+
+    public function view($view, $params = null){
+        /**
+         * @view parametro por donde llega la vista que se desea cargar dentro del layout
+         * @param valores que se cargan en la vista (opcional iniciando como nulo)
+         * @data['content'] arreglo que se mandara a llamar desde el contendor del layout
+         * @data['id_usuario'] arreglo que almacenara el id del usuario logeado y que podra ser heredado
+         * @data['username'] arreglo que almacenara el nombre del usuario logeado y que podra ser heredado
+         * @data['permisos'] arreglo que almacenara el permiso del usuario logeado y que podra ser heredado
+         * @data['titulo'] arreglo que almacenara el titulo de la vista y que podra ser heredado
+         */
+
+
+        $data = array();
+        $params["username"] = $this->name_user;
+        $params["permisos"] = $this->permisos;
+        $params["titulo"] = "AppWeb | ".$view;
+        $data['content'] = $this->load->view('vistas/'.$view, $params, true);
+        
+        if ($view == "login") {
+            $this->session->sess_destroy();
+            $this->load->view('layout/main_layout',$data, false);
+        }
+        else {
+            $this->load->view('layout/head',$data);
+            $this->load->view('layout/nav');
+            $this->load->view('layout/header');
+            $this->load->view('layout/main_layout',$data, false);
+            $this->load->view('layout/footer');
+        }
+    }
+    function hola(){
+        echo "hola";
+    }
+    
+}
---
+++ application/controllers/Login.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Login extends CI_Controller {
+
+    public function __construct(){
+        parent::__construct();
+        $this->load->model("login_m",'',TRUE);
+        $this->load->library('session');
+        $this->load->helper('url');
+    }
+    public function index()
+    {
+        $this->load->view("login");
+    }
+    public function validacion()
+    {
+        $username = $this->input->post("username");
+        $password = $this->input->post("password");
+        
+        $result = $this->login_m->validacion($username,MD5($password));
+        if($result !== FALSE){
+            $this->session->set_userdata(["login_state"=>TRUE]);
+            $this->session->set_userdata('datos_usuario',$result);
+            echo TRUE;
+        }else{
+            $this->session->set_userdata(["login_state"=>FALSE]);
+            echo FALSE;
+        }
+    }
+    public function logout()
+    {
+        $this->session->sess_destroy();
+        $this->index();
+    }
+}
