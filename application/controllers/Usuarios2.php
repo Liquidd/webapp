ce2a6117173dfa94f9b30984ef205e9f63d64526
Luis Manuel Rojo de Santiago <luis_rojo240@hotmail.com>
2019-11-14 11:50:18 -0700

    correccion nombre de archivos

---
+++ application/controllers/Usuarios2.php
@@ -0,0 +1,66 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+include_once('controlador_general.php');
+class Usuarios2 extends Controlador_general {
+
+    public function __construct(){
+        parent::__construct();
+        $this->load->model("usuarios_m",'',TRUE);
+        $this->load->helper('url');
+        $this->load->library('session');
+        if (!$this->estado_sesion) {
+            $this->session->sess_destroy();
+            redirect("login");
+        }
+    }
+
+    public function index()
+    {
+            $lista_usuarios = $this->usuarios_m->lista_usuarios();
+            $array_usuarios = array();
+    
+            if($lista_usuarios !== FALSE)
+            foreach ($lista_usuarios as $key => $values) {
+                    $array_usuarios[$key]['id_usuario']=$values['id_usuario'];
+                    $array_usuarios[$key]['username']=$values['username'];
+                    $array_usuarios[$key]['password']=$values['password'];
+                    $array_usuarios[$key]['permisos']=$values['permisos'];
+                    $array_usuarios[$key]['estado']=$values['estado'];
+    
+            }
+            $this->view('inicio',array("usuarios" =>$array_usuarios));
+    }
+
+    public function alta_usuario()
+    {
+        $data = $this->input->post("data");
+        $respuesta = $this->usuarios_m->alta_usuario($data["username"],$data["password"],$data["permisos"]);
+        echo $respuesta;
+    }
+    public function actualizar_usuario()
+    {
+        $data = $this->input->post("data");
+        $id_usuario = $this->input->post("id_usuario");
+        $respuesta = $this->usuarios_m->actualizar_usuario($data["username"],$data["password"],$data["permisos"],$id_usuario);
+        echo $respuesta;
+    }
+    public function estado_usuario()
+    {
+        $id_usuario = $this->input->post("id_usuario");
+        $estado = $this->input->post("estado");
+        $respuesta = $this->usuarios_m->estado_usuario($id_usuario,$estado);
+        echo $respuesta;
+
+    }
+    public function detalle()
+    {
+		$id_usuario = $this->input->post("id_usuario");
+        $respuesta = $this->usuarios_m->lista_usuarios($id_usuario);
+        echo json_encode($respuesta[0]);
+    }
+    public function holaUsuarios()
+    {
+        echo "hola usuarios";
+    }
+
+}
