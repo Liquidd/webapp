9d3340ce88096b551b31fc554e1db3e5c9eb414f
Luis Manuel Rojo de Santiago <luis_rojo240@hotmail.com>
2019-11-16 01:17:10 -0700

    test config sesion

--- application/config/config.php
+++ application/config/config.php
@@ -380,10 +380,10 @@ $config['encryption_key'] = '';
 */
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 1440;
+$config['sess_expiration'] = 7200;
 $config['sess_save_path'] = FCPATH . 'application/cache/sessions/';
-$config['sess_match_ip'] = TRUE;
-$config['sess_time_to_update'] = 3600;
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = TRUE;
 
 /*
