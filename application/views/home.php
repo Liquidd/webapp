a9382c7346a2710b88deff27184400e38cef716f
Luis Manuel Rojo de Santiago <luis_rojo240@hotmail.com>
2019-11-24 21:26:18 -0700

    correccion urls home

--- application/views/home.php
+++ application/views/home.php
@@ -5,18 +5,18 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Safario Travel - Home</title>
-	<link rel="icon" href="img/Fevicon.png" type="image/png">
+	<link rel="icon" href="<?= base_url()?>safario/img/Fevicon.png" type="image/png">
 
-  <link rel="stylesheet" href="<?= base_url()?>vendors/bootstrap/bootstrap.min.css">
-  <link rel="stylesheet" href="<?= base_url()?>vendors/fontawesome/css/all.min.css">
-  <link rel="stylesheet" href="<?= base_url()?>vendors/themify-icons/themify-icons.css">
-  <link rel="stylesheet" href="<?= base_url()?>vendors/linericon/style.css">
-  <link rel="stylesheet" href="<?= base_url()?>vendors/owl-carousel/owl.theme.default.min.css">
-  <link rel="stylesheet" href="<?= base_url()?>vendors/owl-carousel/owl.carousel.min.css">
-  <link rel="stylesheet" href="<?= base_url()?>vendors/flat-icon/font/flaticon.css">
-  <link rel="stylesheet" href="<?= base_url()?>vendors/nice-select/nice-select.css">
+  <link rel="stylesheet" href="<?= base_url()?>safario/vendors/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="<?= base_url()?>safario/vendors/fontawesome/css/all.min.css">
+  <link rel="stylesheet" href="<?= base_url()?>safario/vendors/themify-icons/themify-icons.css">
+  <link rel="stylesheet" href="<?= base_url()?>safario/vendors/linericon/style.css">
+  <link rel="stylesheet" href="<?= base_url()?>safario/vendors/owl-carousel/owl.theme.default.min.css">
+  <link rel="stylesheet" href="<?= base_url()?>safario/vendors/owl-carousel/owl.carousel.min.css">
+  <link rel="stylesheet" href="<?= base_url()?>safario/vendors/flat-icon/font/flaticon.css">
+  <link rel="stylesheet" href="<?= base_url()?>safario/vendors/nice-select/nice-select.css">
 
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="<?= base_url()?>css/style.css">
 </head>
 <body class="bg-shape">
 
